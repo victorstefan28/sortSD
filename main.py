9aaa8d821cbcfb24b249b07cc507bdaafbf1a4a5
victorstefan28 <116149427+victorstefan28@users.noreply.github.com>
2023-03-10 00:42:47 +0200

    ->shell sort ->merge sort ->insertion sort ->quick sort implementati
    to do->rezultate teste in fisier pt realizare grafice
    -> ce se intampla daca un alg nu poate sorta-> cum il oprim?(fara seg fault..)

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import random
+import sys
 import time
+import math
 def sort_numarare(v, n, mod): # pt radix
     fr = [0] * mod
     out = [0] * (len(v))
@@ -13,26 +15,123 @@ def sort_numarare(v, n, mod): # pt radix
         fr[(v[i]//n)%mod] -= 1
     for i in range(len(v)):
         v[i] = out[i]
-def sort_num(v):
-    sort_numarare(v, max(v), max(v))
+
+
+def insertion_sort(v):
+    for i in range(len(v)):
+        x = v[i]
+        j = i-1
+        while j>=0 and x < v[j]:
+            v[j+1] = v[j]
+            j-=1
+        v[j+1] = x
+
 
 def sortat(v):
     for i in range(1,len(v)):
         if v[i-1] > v[i]:
             return "Nu"
     return "Da"
+
+def merge_sort(v):
+    if len(v) > 1:
+        mj = len(v)//2
+        st = v[:mj]
+        dr = v[mj:]
+        merge_sort(st);
+        merge_sort(dr);
+        # sortare prin interclasare ->
+        i = 0
+        j = 0
+        k = 0
+        while i < len(st) and j < len(dr):
+            if st[i] < dr[j]:
+                v[k] = st[i]
+                i+=1
+            else:
+                v[k] = dr[j]
+                j+=1
+            k+=1
+        #una din liste ramane cu elemente la final -> le adaugam
+        while j<len(dr):
+            v[k] = dr[j]
+            j+=1
+            k+=1
+        while i<len(st):
+            v[k] = st[i]
+            i+=1
+            k+=1
+
+def partitie(v, st, dr):
+    mj = (st + dr) // 2
+    pivot = 0
+    if v[mj] < v[st]:
+        v[st], v[mj] = v[mj], v[st]
+    if v[dr] < v[st]:
+        v[st], v[dr] = v[dr], v[st]
+    if v[mj] < v[dr]:
+        v[mj], v[dr] = v[dr], v[mj]
+    #mediana din 3
+    pivot = v[dr]
+
+    i = st-1
+
+
+    for j in range(st,dr):
+        if v[j]<=pivot:
+            i+=1
+            v[i],v[j]=v[j],v[i]
+    v[i+1],v[dr]=v[dr],v[i+1]
+
+    return i+1
+
+def shell_sort(v):
+    k = len(v)//2
+    while k>0:
+        for i in range(k,len(v)):
+            temp = v[i]
+            j = i
+            while j>=k and v[j-k]>temp:
+                v[j] = v[j-k]
+                j-=k
+            v[j] = temp
+        k//=2
+def q_sort(v, st, dr):
+    if(st<dr):
+        part_ind = partitie(v, st, dr)
+
+        q_sort(v, st, part_ind-1)
+        q_sort(v, part_ind+1, dr)
+def q_sort_call(v):
+    if sortat(v) == "Da":
+        return
+    if (max(v)-min(v)<=2 and len(v) > 10**4) or (max(v)-min(v)<=1 and len(v) > 10**3):
+        print("Q-sort nu poate sorta->recursion stack overflow") #
+        return "fail"
+    q_sort(v,0,len(v)-1)
+
 def radix_sort(v):
     nmax = max(v)
     p = 1
-    _radix = 10
+    putere_doi = 1
+    print(nmax)
+    x = math.sqrt(nmax)
+    while x>2:
+        putere_doi+=1
+        x//=2
+    print(putere_doi, 2**putere_doi)
+    #alegem o baza favorabila-> observam ca pt numere mari bazele mici, 2^2,3,4,5 nu dau rezultate decente, nu vrem sa impartim nici de prea multe ori, nici de prea putine
+    #daca alegem ca puterea lui 2 sa fie nr de factori 2 din radicalul maximului garantim minim o impartire(?)
+    _radix = max(2**putere_doi, 8)
     while nmax // p > 0:
         sort_numarare(v, p, _radix)
-        p *= _radix
+        p = p << putere_doi
 
 
 def main():
     f = open("teste.in")
-    T = f.readline()
+    sys.setrecursionlimit(10**9)# pt q-sort
+    T = int(f.readline())
     teste = []
     for line in f.readlines():
         n, valmx = line.split()
@@ -41,15 +140,17 @@ def main():
         teste.append((int(n), int(valmx)))
     print(teste)
 
-    sortari = [radix_sort]
+    sortari = [radix_sort, merge_sort, q_sort_call, shell_sort, insertion_sort]
     for test in teste:
-        init = [random.randint(1, test[1]) for _ in range(test[0])]
+        init = [random.randint(0, test[1]) for _ in range(test[0])]
         for sortare in sortari:
             L = init.copy()
             inc = time.time()
-            sortare(L)
+            rez = sortare(L)
             sf = time.time()
-            print(f"{str(sortare.__name__)} a durat {round(sf-inc, 5)} secunde pentru {test[0]} valori in intervalul [1, {test[1]}], {sortat(L)}")
+            if rez!="fail":
+                print(f"{(str(sortare.__name__)).split('_sort')[0]}-sort a durat {round(sf-inc, 5)} secunde pentru {test[0]} valori in intervalul [1, {test[1]}], {sortat(L)}")
+
         inc = time.time()
         init.sort()
         sf = time.time()
